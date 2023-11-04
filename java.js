77bc7e8071bacff48a12479ed3adc701917c2e38
Rafael Polanco <scom2016@hotmail.com>
2023-11-04 10:46:17 -0500

    primera subida

---
+++ java.js
@@ -0,0 +1,101 @@
+/* ejercicio 1 */
+/*
+let numero1 = prompt ("introduce un numnero")
+console.log(numero1);
+let numero2 = prompt ("introduce otro numero")
+console.log(numero2);
+if (numero1==numero2) {console.log ("Los numeros son iguales")  
+}
+ else if (numero1>numero2){ console.log ("El pimero numero es mayor")} 
+ else {console.log("El segundo numero es mayor")
+ } 
+
+ /* ejercicio 2 */
+ /*
+ let numero3 = prompt ("introduce un numnero")
+console.log(numero3);
+let numero4 = prompt ("introduce otro numero")
+console.log(numero4);
+if (numero3==numero4) {console.log ("Los numeros son iguales")  
+}
+ else {console.log("Los numeros son diferentes")
+ } 
+/* ejercicio 3 */
+/*
+let numero5 = prompt ("introduce un numnero")
+console.log(numero5);
+let numero6 = prompt ("introduce otro numero")
+console.log(numero6);
+if (numero5==numero6) {console.log ("Los numeros son iguales")  
+} else if(numero5>numero6) {console.log ("El primer numero es mayor: " +numero5)  
+}
+ else {console.log("El segundo numero es mayor: "+numero6 )
+ } 
+/* ejercicio 4 */
+/*
+let numero6 = prompt ("introduce primer numnero")
+console.log(numero6);
+let numero7 = prompt ("introduce segundo numero")
+console.log(numero7);
+let numero8 = prompt ("introduce tercero numero")
+console.log(numero8);
+let resultado= Math.min (numero6,numero7,numero8);
+console.log("El numero menor es: "+resultado)
+
+/*ejercicio 5 */
+/*
+let nombrepersona1 = prompt ("introduce nombre primera persona")
+let edadpersona1 = prompt ("introduce edad primera persona")
+let alturapersona1 = prompt ("introduce altura primera persona")
+let nombrepersona2 = prompt ("introduce nombre segunda persona")
+let edadpersona2 = prompt ("introduce edad segunda persona")
+let alturapersona2 = prompt ("introduce altura segunda persona")
+
+if  (alturapersona1>alturapersona2){console.log(nombrepersona1 +" es mas alto:" + alturapersona1 + "cm");}
+else{console.log(nombrepersona2 +" es mas alto:" + alturapersona2 + "cm");}
+if  (edadpersona1>edadpersona2){console.log(nombrepersona1 +" es mayor:" + edadpersona1 + "años");}
+else{console.log(nombrepersona2 +" es mayor:" + edadpersona2 + "años");}
+
+/* ejercicio 6 */
+/*
+let nombreperson3 = prompt ("introduce nombre de la persona")
+let edadpersona3 = prompt ("introduce edad de la  persona")
+let alturaperson3 = prompt ("introduce altura de la  persona")
+let visionpersona3 = prompt ("vision de la  persona")
+
+let licencia = (edadpersona3>=18)&&(alturaperson3>=150)&&(visionpersona3>=8)
+if (licencia==true) {
+   console.log("Puede manejar");
+} else{ console.log("No puede manejar");}
+
+/* ejercicio 7 */
+
+let nombreperson4 = prompt ("introduce nombre de la persona")
+let pase = prompt ("tipo de pase")
+let entrada= prompt ("Posee entrada")
+let entradasi= "si"
+
+if (nombreperson4=="rafael)") {console.log("Bienvenido pase")
+}else if(pase=="vip"){console.log(" Bienvenido pase") 
+}
+if (pase=="normal"){console.log("Bienvenido, espere un momento")
+} else if (entrada=="no"){console.log("Adios");}
+
+if (entrada=="si"){ prompt("Decea usarlo?") 
+}else if(entradasi=="si") { console.log("Bienvenido pase");}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
