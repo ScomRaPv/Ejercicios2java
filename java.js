7accddee913728dbd489112eaee70c1b5de3f33c
Rafael Polanco <scom2016@hotmail.com>
2023-11-08 17:54:00 -0500

    actualizacion

--- java.js
+++ java.js
@@ -1,5 +1,5 @@
 /* ejercicio 1 */
-/*
+
 let numero1 = prompt ("introduce un numnero")
 console.log(numero1);
 let numero2 = prompt ("introduce otro numero")
@@ -11,7 +11,7 @@ if (numero1==numero2) {console.log ("Los numeros son iguales")
  } 
 
  /* ejercicio 2 */
-/*
+
 let numero3 = prompt ("introduce un numnero")
 console.log(numero3);
 let numero4 = prompt ("introduce otro numero")
@@ -21,7 +21,7 @@ if (numero3==numero4) {console.log ("Los numeros son iguales")
 else {console.log("Los numeros son diferentes")
 } 
 /* ejercicio 3 */
-/*
+
 let numero5 = prompt ("introduce un numnero")
 console.log(numero5);
 let numero6 = prompt ("introduce otro numero")
@@ -32,7 +32,7 @@ if (numero5==numero6) {console.log ("Los numeros son iguales")
  else {console.log("El segundo numero es mayor: "+numero6 )
  } 
 /* ejercicio 4 */
-/*
+
 let numero6 = prompt ("introduce primer numnero")
 console.log(numero6);
 let numero7 = prompt ("introduce segundo numero")
@@ -43,7 +43,7 @@ let resultado= Math.min (numero6,numero7,numero8);
 console.log("El numero menor es: "+resultado)
 
 /*ejercicio 5 */
-/*
+
 let nombrepersona1 = prompt ("introduce nombre primera persona")
 let edadpersona1 = prompt ("introduce edad primera persona")
 let alturapersona1 = prompt ("introduce altura primera persona")
@@ -57,7 +57,7 @@ if  (edadpersona1>edadpersona2){console.log(nombrepersona1 +" es mayor:" + edadp
 else{console.log(nombrepersona2 +" es mayor:" + edadpersona2 + "años");}
 
 /* ejercicio 6 */
-/*
+
 let nombreperson3 = prompt ("introduce nombre de la persona")
 let edadpersona3 = prompt ("introduce edad de la  persona")
 let alturaperson3 = prompt ("introduce altura de la  persona")
@@ -69,23 +69,32 @@ if (licencia==true) {
 } else{ console.log("No puede manejar");}
 
 /* ejercicio 7 *///////////////////////////////////////////
-/*
-let nombreperson4 = prompt ("introduce nombre de la persona")
-let pase = prompt ("tipo de pase")
-let entrada= prompt ("Posee entrada")
-let entradasi= "si"
 
-if (nombreperson4=="rafael)") {console.log("Bienvenido pase")
-}else if(pase=="vip"){console.log(" Bienvenido pase") 
-}
-if (pase=="normal"){console.log("Bienvenido, espere un momento")
-} else if (entrada=="no"){console.log("Adios");}
+ const nombre = prompt("Ingrese su nombre: ");
+    const pase = prompt("¿Tiene pase VIP o normal? (vip/normal): ").toLowerCase();
+    const tieneEntrada = prompt("¿Posee entrada? (si/no): ").toLowerCase();
 
-if ((entrada=="si")){ prompt("Decea usarlo?") 
-}else if((entrada=="si")&&(entradasi== "si")) { console.log("Bienvenido pase");}
+    if (nombre === "TuNombre" || pase === "vip") {
+        console.log("¡Bienvenido! Disfrute del evento.");
+        if (tieneEntrada === "si" || tieneEntrada === "s" || tieneEntrada === "true") {
+            console.log("¡Bienvenido! Gracias por utilizar su entrada.");
+        }
+    } else {
+        const comprar = prompt("¿Desea comprar una entrada? (si/no): ").toLowerCase();
+        if (comprar === "si" || comprar === "s") {
+            const dineroDisponible = parseInt(prompt("Ingrese el dinero disponible: "));
+            if (dineroDisponible >= 1000) {
+                console.log("¡Venta de entrada exitosa! ¡Bienvenido!");
+            } else {
+                console.log("Lo siento, no tiene suficiente dinero para comprar la entrada.");
+            }
+        } else {
+            console.log("Que tenga un buen día. ¡Hasta luego!");
+        }
+    }
 
 /* Ejercicio 8*/
-/*
+
 let numeroincognita = 7
 let numeroingresado = prompt("Introduce un numero del 1 al 10") 
 let intentos = 2
@@ -115,7 +124,7 @@ if (numeroincognita == numeroingresado3 ) {
 }
 
 /* ejercicio 9 */
-/*
+
  let ingresatuedad= prompt("Ingresa tu edad")
  
  if(0>=ingresatuedad,12>=ingresatuedad){console.log("Eres un infante");
@@ -135,8 +144,6 @@ let opcionUsuario2;
 opcionUsuario = prompt("¿Qué eliges jugador1?\nPiedra: 0\nPapel: 1\nTijera: 2", 0);
 
 opcionUsuario2 = prompt("¿Qué eliges jugador2?\nPiedra: 0\nPapel: 1\nTijera: 2", 0);
-
-
 
 alert(" jugador1 eligio " + opciones[opcionUsuario]);
 alert("jugador2 eligió " + opciones[opcionUsuario2]);
@@ -187,6 +194,95 @@ else if(opcionUsuario == tijera)
         alert("Empate!");
     }  else {alert ("Uno de los jugadores hizo trampa");}
 }
+
+/* Ejercico 11 */
+
+let color = prompt("Ingrese un color")
+
+switch (color) {
+   case "blanco":
+      console.log("Falta de color")
+      break;
+   case "negro":
+      console.log("Falta de color")
+      break;
+   case "verde":
+      console.log("El color de la naturaleza")
+      break;
+   case "azul":
+      console.log(" El color del agua")
+      break;
+   case "amarillo":
+      console.log("El color del sol")
+      break;
+   case "rojo":
+      console.log("El color del fuego")
+      break;
+   case "Marrón":
+      console.log(" el color de la tierra")
+
+   default:
+      break;
+}
+
+/* ejercicio 12 */
+
+let num1 = parseFloat(prompt("Ingrese el primer número:"));
+let num2 = parseFloat(prompt("Ingrese el segundo número:"));
+let operacion = prompt("Ingrese la operación deseada (suma, resta, multiplicacion, division):").toLowerCase();
+
+ad
+switch (operacion) {
+    case 'suma':
+        resultado = num1 + num2;
+        console.log(`El resultado de la suma es: ${resultado}`);
+        break;
+    case 'resta':
+        resultado = num1 - num2;
+        console.log(`El resultado de la resta es: ${resultado}`);
+        break;
+    case 'multiplicacion':
+        resultado = num1 * num2;
+        console.log(`El resultado de la multiplicación es: ${resultado}`);
+        break;
+    case 'division':
+        if (num2 === 0) {
+            console.log("ERROR: No se puede dividir entre cero.");
+        } else {
+            resultado = num1 / num2;
+            console.log(`El resultado de la división es: ${resultado}`);
+        }
+        break;
+    default:
+        console.log("Operación no válida. Intente de nuevo.");
+}
+ /* Ejercicio 13 */
+
+ 
+ const CCdatos = {
+   numero: prompt("Ingrese el número de su CC:"),
+   nombres: prompt("Ingrese su nombre completo:"),
+   fechaNacimiento: prompt("Ingrese su fecha de nacimiento (DD/MM/AAAA):"),
+   direccion: prompt("Ingrese su dirección:"),
+   departamento: prompt("Ingrese su departamento:")
+};
+    console.log(CCdatos);
+
+
+    const confirmacion = confirm("¿Son correctos los datos ingresados?");
+
+    if (confirmacion) {
+        const CC = { CCdatos };
+        console.log("Registro exitoso. A continuación se muestra el CC registrado:");
+        console.table(CC);
+    } else {
+        console.log("Vuelva a intentarlo en 1 mes.");
+    }
+
+
+
+
+
 
 
 
